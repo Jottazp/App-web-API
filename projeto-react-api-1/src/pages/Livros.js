6715bb50c1242eefde309e9fd11befbfba8bbc95
Jottazp <contato.rodrigues5000@gmail.com>
2024-04-25 20:39:47 -0300

    LISTANDO LIVROS

--- projeto-react-api-1/src/pages/Livros.js
+++ projeto-react-api-1/src/pages/Livros.js
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+
 import Message from '../components/message/Message';
+import CardBook from '../components/cardBook/CardBook'
 import styles from './Livros.module.css'
 
 function Livros() {
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(()=>{
+
+    fetch('http://localhost:5000/books', {
+      method: 'GET',
+      headers: {
+        'Content-Type' : 'application.json'
+      },
+    })
+    .then((resp)=>resp.json())
+    .then((data)=>{setBooks(data)})
+    .catch((err)=>{console.log(err)});
+
+  }, []);
 
   const location = useLocation();
   let message='';
@@ -19,12 +38,26 @@ function Livros() {
       <h1>Aqui serão listados os seus livros</h1>
 
       {
-        message && (
-          <Message
-            msg={message}
-            type='success'
-          /> 
+        message && (<Message
+                      msg={message}
+                      type='success'
+                    /> 
         )
+      }
+
+      {
+
+       books.map((book)=>(
+        
+        <CardBook
+          id={book.id}
+          livro={book.nome_livro}
+          autor={book.nome_autor}
+          categoria={book.category.category}
+        />
+
+       )) 
+
       }
 
     </section>
